eb03a3c7286ae30fc90be60416d8c1d00c129770
Dan <DanielMooreRealPerson@gmail.com>
2021-05-29 19:13:39 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+let element = document.querySelector("main");
+element.remove(main);
+
+let newHeader = document.createElement('h1');
+newHeader.id = "victory"
+newHeader.innerHTML = "Dan is the champion!"
+newHeader.className = "victory"
